dd96e5359ce1253d2fd2c421f49eb412894960cc
Tharuka_00 <tharusenevi2000@gmail.com>
2023-08-31 13:38:31 +0530

    erosion and dilation process

--- src/TableExtractor.py
+++ src/TableExtractor.py
@@ -54,20 +54,27 @@ class TableExtractor:
         vertical_kernel = rectangle(5, 1)
         dilated_image_array = image_array.copy()  # Make a copy to preserve original
         for _ in range(iterations):
-            dilated_image_array = binary_erosion(
+            dilated_image_array = binary_dilation(
                 dilated_image_array, vertical_kernel)
         self.v_dilated_image = Image.fromarray(dilated_image_array)
 
+    # extracting horizontal lines
     def h_erosion_image(self, iterations=5):
-        image_array = np.array(self.v_dilated_image)
+        image_array = np.array(self.inverted_image)
         horizontal_kernel = rectangle(1, 5)
-        eroded_image_array = binary_erosion(image_array, horizontal_kernel)
+        eroded_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            eroded_image_array = binary_erosion(
+                eroded_image_array, horizontal_kernel)
         self.h_eroded_image = Image.fromarray(eroded_image_array)
 
     def h_dilation_image(self, iterations=5):
         image_array = np.array(self.h_eroded_image)
         horizontal_kernel = rectangle(1, 5)
-        dilated_image_array = binary_dilation(image_array, horizontal_kernel)
+        dilated_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            dilated_image_array = binary_dilation(
+                dilated_image_array, horizontal_kernel)
         self.h_dilated_image = Image.fromarray(dilated_image_array)
 
     '''
@@ -111,10 +118,10 @@ class TableExtractor:
             "./uploads/5_vertical_dilated.jpg", self.v_dilated_image)
         self.h_erosion_image(iterations=5)
         self.store_process_image(
-            "./uploads/6_horizontal_eroded.jpg", self.v_eroded_image)
+            "./uploads/6_horizontal_eroded.jpg", self.h_eroded_image)
         self.h_dilation_image(iterations=5)
         self.store_process_image(
-            "./uploads/7_horizontal_dilated.jpg", self.v_dilated_image)        # self.dilate_image()
+            "./uploads/7_horizontal_dilated.jpg", self.h_dilated_image)        # self.dilate_image()
         # self.store_process_image(
     #     "./uploads/_dialateded.jpg", self.dilated_image)
         self.find_contours()
