69b5e87ea987150f2b373be6a7adf5fc195376e7
Tharuka_00 <tharusenevi2000@gmail.com>
2023-09-11 16:01:34 +0530

    Finalizing Table Extractor

--- src/TableExtractor.py
+++ src/TableExtractor.py
@@ -1,7 +1,4 @@
-from PIL import Image, ImageOps, ImageFilter, ImageDraw
-from skimage import measure
-from skimage.measure import find_contours
-from skimage.measure import regionprops, label
+from PIL import Image, ImageOps
 from skimage.morphology import rectangle, binary_erosion, binary_dilation
 import numpy as np
 import matplotlib.pyplot as plt
@@ -207,6 +204,30 @@ class TableExtractor:
                 image_array, point_coordinates, 10, (0, 0, 255), -1)
             self.image_with_points_plotted = Image.fromarray(image_array)
 
+    def order_points(self, pts):
+        # initialzie a list of coordinates that will be ordered
+        # such that the first entry in the list is the top-left,
+        # the second entry is the top-right, the third is the
+        # bottom-right, and the fourth is the bottom-left
+        pts = pts.reshape(4, 2)
+        rect = np.zeros((4, 2), dtype="float32")
+
+        # the top-left point will have the smallest sum, whereas
+        # the bottom-right point will have the largest sum
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+
+        # return the ordered coordinates
+        return rect
+
     def calculate_new_width_and_height_of_image(self):
         existing_image_width, existing_image_height = self.image.size
         existing_image_width_reduced_by_10_percent = int(
@@ -238,29 +259,16 @@ class TableExtractor:
             src_image, matrix, (self.new_image_width, self.new_image_height))
         self.perspective_corrected_image = Image.fromarray(img_array)
 
-    def order_points(self, pts):
-        # initialzie a list of coordinates that will be ordered
-        # such that the first entry in the list is the top-left,
-        # the second entry is the top-right, the third is the
-        # bottom-right, and the fourth is the bottom-left
-        pts = pts.reshape(4, 2)
-        rect = np.zeros((4, 2), dtype="float32")
-
-        # the top-left point will have the smallest sum, whereas
-        # the bottom-right point will have the largest sum
-        s = pts.sum(axis=1)
-        rect[0] = pts[np.argmin(s)]
-        rect[2] = pts[np.argmax(s)]
-
-        # now, compute the difference between the points, the
-        # top-right point will have the smallest difference,
-        # whereas the bottom-left will have the largest difference
-        diff = np.diff(pts, axis=1)
-        rect[1] = pts[np.argmin(diff)]
-        rect[3] = pts[np.argmax(diff)]
-
-        # return the ordered coordinates
-        return rect
+    def add_10_percent_padding(self):
+        image_width, image_height = self.perspective_corrected_image.size
+        padding = int(image_height * 0.1)
+        # Create a white background image with the desired dimensions
+        padded_image = Image.new(
+            'RGB', (image_width + 2 * padding, image_height + 2 * padding), (255, 255, 255))
+        # Paste the perspective corrected image onto the white background with padding
+        padded_image.paste(self.perspective_corrected_image,
+                           (padding, padding))
+        self.perspective_corrected_image_with_padding = padded_image
 
     def execute(self):
         self.read_image()
@@ -290,9 +298,6 @@ class TableExtractor:
         self.blend_images(1, 1)
         self.store_process_image(
             "./uploads/8_blended.jpg", self.blended_image)
-        # self.erosion_blended_image(iterations=1)
-        # self.store_process_image(
-        #    "./uploads/9_eroded_blended.jpg", self.eroded_blended_image)
         self.threshold_blended_image()
         self.store_process_image(
             "./uploads/9_thresholded_blended.jpg", self.thresh_blended_image)
@@ -312,3 +317,7 @@ class TableExtractor:
         self.apply_perspective_transform()
         self.store_process_image(
             "./uploads/14_perspective_corrected.jpg", self.perspective_corrected_image)
+        self.add_10_percent_padding()
+        self.store_process_image("./uploads/15_perspective_corrected_with_padding.jpg",
+                                 self.perspective_corrected_image_with_padding)
+        return self.perspective_corrected_image_with_padding
