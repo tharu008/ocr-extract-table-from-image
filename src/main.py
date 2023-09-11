0687ef4e23fa9adce3e64c2f7ed87074be45e0ad
Tharuka_00 <tharusenevi2000@gmail.com>
2023-09-11 13:02:01 +0530

    image update

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ import TableExtractor as te
 import TableLinesRemover as tlr
 
 
-path_to_image = "D:\\University\\YEAR 04 SEM 02\\CGV\\Assignment\\image-processing-model\\src\\uploads\\1.jpg"
+path_to_image = "D:\\University\\YEAR 04 SEM 02\\CGV\\Assignment\\image-processing-model\\src\\uploads\\1sample.jpeg"
 table_extractor = te.TableExtractor(path_to_image)
 table_extractor.execute()
